d6aa2f6ba909c432276889e1514adcf84dbef782
theifedayo <adesiyanifedayo@gmail.com>
2023-07-23 16:06:01 +0100

    add get cache key

---
+++ dumb_password.go
@@ -0,0 +1,67 @@
+package dumbpassword
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"io/ioutil"
+	"strings"
+	"sync"
+)
+
+// DumbPasswordValidator is a struct representing the Dumb Password Validator.
+type DumbPasswordValidator struct {
+	passwordListPath string
+	commonPasswords  []string
+	mutex            sync.Mutex
+}
+
+// NewDumbPasswordValidator creates a new instance of DumbPasswordValidator.
+func NewDumbPasswordValidator(passwordListPath string) (*DumbPasswordValidator, error) {
+	commonPasswords, err := loadPasswordList(passwordListPath)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DumbPasswordValidator{
+		passwordListPath: passwordListPath,
+		commonPasswords:  commonPasswords,
+	}, nil
+}
+
+// Validate checks if the given password is common (dumb) and returns true if it's not common.
+func (v *DumbPasswordValidator) Validate(password string) bool {
+	v.mutex.Lock()
+	defer v.mutex.Unlock()
+
+	lowercasePassword := strings.ToLower(password)
+	for _, commonPassword := range v.commonPasswords {
+		if lowercasePassword == commonPassword {
+			return false
+		}
+	}
+	return true
+}
+
+// loadPasswordList loads the list of common passwords from the file and returns it as a slice of strings.
+func loadPasswordList(passwordListPath string) ([]string, error) {
+	data, err := ioutil.ReadFile(passwordListPath)
+	if err != nil {
+		return nil, err
+	}
+	passwords := strings.Split(string(data), "\n")
+	var commonPasswords []string
+	for _, password := range passwords {
+		commonPasswords = append(commonPasswords, strings.ToLower(password))
+	}
+	return commonPasswords, nil
+}
+
+// GetCacheKey calculates the cache key based on the MD5 hash of the password list file content.
+func (v *DumbPasswordValidator) GetCacheKey() (string, error) {
+	data, err := ioutil.ReadFile(v.passwordListPath)
+	if err != nil {
+		return "", err
+	}
+	hash := md5.Sum(data)
+	return hex.EncodeToString(hash[:]), nil
+}
---
+++ dumb_password_test.go
@@ -0,0 +1,89 @@
+package dumbpassword
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestDumbPasswordValidator_Validate(t *testing.T) {
+	// Create a temporary password list file for testing.
+	passwordListContent := "password1\npassword2\npassword3\n"
+	tempFile, err := ioutil.TempFile("", "passwordlist.txt")
+	if err != nil {
+		t.Fatalf("Error creating temporary file: %v", err)
+	}
+	defer os.Remove(tempFile.Name())
+
+	// Write the test password list to the temporary file.
+	_, err = tempFile.WriteString(passwordListContent)
+	if err != nil {
+		t.Fatalf("Error writing to temporary file: %v", err)
+	}
+
+	// Create a new DumbPasswordValidator instance.
+	validator, err := NewDumbPasswordValidator(tempFile.Name())
+	if err != nil {
+		t.Fatalf("Error creating DumbPasswordValidator: %v", err)
+	}
+
+	// Test common and uncommon passwords.
+	testCases := []struct {
+		password string
+		isCommon bool
+	}{
+		{"password1", false},
+		{"password2", false},
+		{"password3", false},
+		{"securePassword123", true},
+		{"commonpassword", true},
+		{"123456", true},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.password, func(t *testing.T) {
+			isCommon := validator.Validate(tc.password)
+			if isCommon != tc.isCommon {
+				t.Errorf("Expected %v for password %s, but got %v", tc.isCommon, tc.password, isCommon)
+			}
+		})
+	}
+}
+
+// TestDumbPasswordValidator_GetCacheKey tests the GetCacheKey function of DumbPasswordValidator.
+func TestDumbPasswordValidator_GetCacheKey(t *testing.T) {
+	passwordListContent := "password1\npassword2\npassword3\n"
+	tempFile, err := ioutil.TempFile("", "passwordlist.txt")
+	if err != nil {
+		t.Fatalf("Error creating temporary file: %v", err)
+	}
+	defer os.Remove(tempFile.Name())
+
+	_, err = tempFile.WriteString(passwordListContent)
+	if err != nil {
+		t.Fatalf("Error writing to temporary file: %v", err)
+	}
+
+	validator, err := NewDumbPasswordValidator(tempFile.Name())
+	if err != nil {
+		t.Fatalf("Error creating DumbPasswordValidator: %v", err)
+	}
+
+	fileData, err := ioutil.ReadFile(tempFile.Name())
+	if err != nil {
+		t.Fatalf("Error reading file for cache key calculation: %v", err)
+	}
+	expectedKey := md5.Sum(fileData)
+	expectedKeyString := hex.EncodeToString(expectedKey[:])
+
+	actualKey, err := validator.GetCacheKey()
+	if err != nil {
+		t.Fatalf("Error calculating cache key: %v", err)
+	}
+
+	if actualKey != expectedKeyString {
+		t.Errorf("Expected cache key %s, but got %s", expectedKeyString, actualKey)
+	}
+}
